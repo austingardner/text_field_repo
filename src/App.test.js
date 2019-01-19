13ccefb22f25b7c932f166a7aceaffce5f7be81a
austingardner <austingardner66@gmail.com>
2019-01-18 23:05:10 -0500

    I learned how to mock a react component

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { render, fireEvent, cleanup, waitForElement } from "react-testing-library";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render, fireEvent, cleanup } from "react-testing-library";
 
-it('renders without crashing', () => {
-    const { getByTestId } = render(
-       <App/>
-    );
-    const GOOD_ICE_CREAM = "chocolate chip cookie dough";
+jest.mock("@material-ui/core/TextField", () =>
+  "input"
+);
 
-    const textField = getByTestId("test-field");
-    fireEvent.change(textField, { target: { value: GOOD_ICE_CREAM } });
-    expect(textField.value).toEqual(GOOD_ICE_CREAM);
+it("renders without crashing", () => {
+  const { getByTestId } = render(<App />);
+  const GOOD_ICE_CREAM = "chocolate chip cookie dough";
 
-    cleanup();
+  const textField = getByTestId("test-field");
+  fireEvent.change(textField, { target: { value: GOOD_ICE_CREAM } });
+  expect(textField.value).toEqual(GOOD_ICE_CREAM);
+
+  cleanup();
 });
